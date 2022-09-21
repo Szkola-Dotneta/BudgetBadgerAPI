c86cb038a6abef3f0b9c82d7ee23af8a82863162
Michał B <MadTiger2409@users.noreply.github.com>
2022-09-21 10:06:04 +0200

    Added flag for soft delete and property to store date when entity was created.

--- src/BudgetBadgerWebApi.Domain/Entities/Common/BaseEntity.cs
+++ src/BudgetBadgerWebApi.Domain/Entities/Common/BaseEntity.cs
@@ -3,5 +3,7 @@
     public class BaseEntity
     {
         public int Id { get; set; }
+        public bool Deleted { get; set; } = false;
+        public DateOnly CreatedAt { get; set; }
     }
 }
