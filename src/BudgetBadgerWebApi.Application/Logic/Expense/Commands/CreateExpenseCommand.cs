9b12c00b8af9b3a9929bb3f0166e74eda8eddcb1
Michał B <MadTiger2409@users.noreply.github.com>
2022-10-18 18:45:11 +0200

    Working on create command for Expense entity

---
+++ src/BudgetBadgerWebApi.Application/Logic/Expense/Commands/CreateExpenseCommand.cs
@@ -0,0 +1,7 @@
+﻿using BudgetBadgerWebApi.Domain.Enums;
+using MediatR;
+
+namespace BudgetBadgerWebApi.Application.Logic.Expense.Commands
+{
+    public record CreateExpenseCommand(decimal Value, DateTime OccurredAt, ExpenseStatusEnum Status, int CategoryId, int HouseholdId) : IRequest<ExpenseDto>;
+}
