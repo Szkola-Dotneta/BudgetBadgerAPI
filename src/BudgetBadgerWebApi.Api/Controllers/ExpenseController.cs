96429a3bf572b9417aec53e95c6da579a67edf17
Michał B <MadTiger2409@users.noreply.github.com>
2022-12-12 11:06:58 +0100

    Working on controller

---
+++ src/BudgetBadgerWebApi.Api/Controllers/ExpenseController.cs
@@ -0,0 +1,15 @@
+﻿using BudgetBadgerWebApi.Api.Controllers.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetBadgerWebApi.Api.Controllers
+{
+    [Route("webapi/houseHolds/{householdId}/expenses")]
+    public class ExpenseController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetExpensesAsync(int householdId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
