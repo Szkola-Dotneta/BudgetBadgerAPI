04b3339bf988545aa8c080bae34f1a96b925dfad
Michał B <MadTiger2409@users.noreply.github.com>
2023-02-23 10:27:19 +0100

    Created NamedActionParameter

--- src/BudgetBadgerWebApi.Api/ActionParameters/Expense/UpdateExpense.cs
+++ src/BudgetBadgerWebApi.Api/ActionParameters/Expense/UpdateExpense.cs
@@ -1,17 +1,11 @@
-﻿using BudgetBadgerWebApi.Application.Logic.Expense.Commands;
+﻿using BudgetBadgerWebApi.Api.ActionParameters.Common;
+using BudgetBadgerWebApi.Application.Logic.Expense.Commands;
 using BudgetBadgerWebApi.Domain.Enums;
 
 namespace BudgetBadgerWebApi.Api.ActionParameters.Expense
 {
-    public record UpdateExpense
+    public record UpdateExpense : NamedActionParameter
     {
-        private string? name;
-
-        public string Name
-        {
-            get => name;
-            set => name = value?.Trim().ToLowerInvariant();
-        }
         public decimal Value { get; set; }
         public DateTime OccurredAt { get; set; }
         public ExpenseStatusEnum Status { get; set; }
--- src/BudgetBadgerWebApi.Api/ActionParameters/Income/UpdateIncome.cs
+++ src/BudgetBadgerWebApi.Api/ActionParameters/Income/UpdateIncome.cs
@@ -1,16 +1,10 @@
-﻿using BudgetBadgerWebApi.Application.Logic.Income.Commands;
+﻿using BudgetBadgerWebApi.Api.ActionParameters.Common;
+using BudgetBadgerWebApi.Application.Logic.Income.Commands;
 
 namespace BudgetBadgerWebApi.Api.ActionParameters.Income
 {
-    public record UpdateIncome
+    public record UpdateIncome : NamedActionParameter
     {
-        private string? name;
-
-        public string Name
-        {
-            get => name;
-            set => name = value?.Trim().ToLowerInvariant();
-        }
         public decimal Value { get; set; }
         public DateTime OccurredAt { get; set; }
         public int CategoryId { get; set; }
