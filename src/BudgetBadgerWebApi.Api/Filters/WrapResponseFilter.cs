cb374d2e6543cab60daab4a9681bc7e8516208d6
Michał B <MadTiger2409@users.noreply.github.com>
2023-04-11 18:33:19 +0200

    Fixed filters

--- src/BudgetBadgerWebApi.Api/Filters/WrapResponseFilter.cs
+++ src/BudgetBadgerWebApi.Api/Filters/WrapResponseFilter.cs
@@ -4,32 +4,31 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BudgetBadgerWebApi.Api.Filters
 {
-    public class WrapResponseFilter : IAsyncAlwaysRunResultFilter
-    {
-        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-        {
-            if (!(context.Result is FileStreamResult))
-            {
-                var wraper = new ResponseWrapObject();
-                var result = (ObjectResult)context.Result;
+	public class WrapResponseFilter : IAsyncAlwaysRunResultFilter
+	{
+		public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+		{
+			if (context.Result is ObjectResult response)
+			{
+				var wraper = new ResponseWrapObject();
 
-                switch (context.HttpContext.Response.StatusCode)
-                {
-                    case >= 400:
-                        wraper.Success = false;
-                        wraper.Error = result.Value;
-                        break;
+				switch (response.StatusCode)
+				{
+					case >= 400:
+						wraper.Success = false;
+						wraper.Error = response.Value;
+						break;
 
-                    default:
-                        wraper.Success = true;
-                        wraper.Value = result.Value;
-                        break;
-                }
+					default:
+						wraper.Success = true;
+						wraper.Value = response.Value;
+						break;
+				}
 
-                context.Result = new ObjectResult(wraper);
-            }
+				context.Result = new ObjectResult(wraper) { StatusCode = response.StatusCode };
+			}
 
-            await next();
-        }
-    }
+			await next();
+		}
+	}
 }
